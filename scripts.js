172191d89e5825a74522dfba673d36508710185c
marlossousa2023 <marlos.sousa@yahoo.com.br>
2023-06-08 00:57:23 -0300

    Projeto sorteador DevClub

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const buttonClick = document.querySelector("button")
+
+function randomSort(){
+    const inputMin = Math.ceil(document.querySelector(".min-input").value)
+    const inputMax = Math.floor(document.querySelector(".max-input").value)
+
+    const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin
+
+    alert(result)
+}
+
+
+
+buttonClick.addEventListener('click', randomSort)
